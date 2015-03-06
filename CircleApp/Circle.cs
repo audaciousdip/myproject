c883163367ecc7e715b318866b0382769f5a384b
taukir4u <promit347@gmail.com>
2015-03-06 13:57:04 +0600

    https://github.com/audaciousdip/myproject.git

---
+++ CircleApp/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Circle
+    {
+        public double Radius { set; get; }
+
+        public double GetDiameter()
+        {
+            return 2*Radius;
+        }
+
+        public double GetPerimeter()
+        {
+            return  2*Math.PI*Radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ CircleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle aCircle=new Circle();
+            Console.WriteLine("input the value ");
+           aCircle.Radius = Convert.ToDouble(Console.ReadLine());
+           Console.WriteLine("Diameter :" + aCircle.GetDiameter()+"\nPerimeter:" + aCircle.GetPerimeter()+"\nArea:" + aCircle.GetArea());
+            
+            Console.ReadKey();
+        }
+        
+    }
+}
